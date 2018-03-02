6476bb8705050260a08ed0fc56c986a1081f9be5
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2018-03-02 23:53:03 +0600

    Add Type bar

---
+++ src/components/TypeInput.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="type-area">
+		<div class="type-input">
+			<input v-model="word" @keyup="typing" type="text" class="input" placeholder="Start typing....">
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: [
+			'correctWord'
+		],
+		data(){
+			return {
+				word: '',
+				typeStarted: false
+			}
+		},
+		methods: {
+			typing(e){
+				if(!this.typeStarted){
+					this.typeStarted = true
+				}
+				let status = this.validateWord(this.correctWord.name, this.word)
+				if(e.keyCode == 32){
+					this.$parent.$emit('updateWordStatus', status ? 'complete' : 'wrong')
+					this.$parent.$emit('changeTypingPosition')
+					this.word = ''
+					return
+				}
+				
+				this.$parent.$emit('updateWordStatus', status ? 'correct' : 'warning');
+			},
+			changeWord(e){
+				this.$parent.$emit('changeTypingPosition')
+				this.word = ''
+				let status = this.validateWord(this.correctWord.name + ' ', this.word)
+				this.$parent.$emit('updateWordStatus', status ? 'correct' : 'wrong');
+			},
+			validateWord(correctWord, typingWord){
+				let regex = `^${typingWord}.*$`
+				return new RegExp(regex).test(correctWord)
+			},
+		}
+	}
+</script>
+<style lang="scss">
+	.type-area{
+		padding: 20px;
+		text-align: center;
+		.type-input{
+			max-width: 450px;
+			margin: auto
+		}
+		.input{
+			padding:15px;
+			width: 100%;
+			box-sizing: border-box;
+			border: 1px solid #ddd;
+			font-size: 19px;
+			font-family: "Operator Mono Book";
+			transition: 0.6s;
+			&:focus{
+				outline: none;
+				border-color: #00a8ff;
+				box-shadow: 0px 0px 0px 4px #00A8FF
+			} 
+		}
+	}
+</style>
